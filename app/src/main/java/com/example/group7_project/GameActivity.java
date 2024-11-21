66711e899e59a05335556724bbbad1c341df7083
PattarataThanaakkarasophon6509611940 <pattarata.tha@dome.tu.ac.th>
2024-11-21 14:00:57 +0700

    add scene

--- app/src/main/java/com/example/group7_project/GameActivity.java
+++ app/src/main/java/com/example/group7_project/GameActivity.java
@@ -16,7 +16,7 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.scene_1t5);
+        setContentView(R.layout.scene_1t4);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -24,8 +24,8 @@ public class GameActivity extends AppCompatActivity {
         });
 
         Button btnNext = findViewById(R.id.btnNext);
-        ImageView imgScene = findViewById(R.id.imgScene_1t5);
-        TextView text = findViewById(R.id.textScene_1t5);
+        ImageView imgScene = findViewById(R.id.imgScene_1t4);
+        TextView text = findViewById(R.id.textScene_1t4);
 
         // ตัวนับสถานะเพื่อเก็บสถานะของ Scene
         final int[] sceneIndex = {1}; // เริ่มต้นที่ Scene 1
@@ -35,23 +35,18 @@ public class GameActivity extends AppCompatActivity {
             switch (sceneIndex[0]) {
                 case 1:
                     imgScene.setImageResource(R.drawable.scene_1_2);
-                    text.setText(R.string.scene_1_2);
+                    text.setText(R.string.Scene_1_2);
                     sceneIndex[0] = 2; // เปลี่ยนเป็น Scene 2
                     break;
                 case 2:
                     imgScene.setImageResource(R.drawable.scene_1_3);
-                    text.setText(R.string.scene_1_3);
+                    text.setText(R.string.Scene_1_3);
                     sceneIndex[0] = 3; // เปลี่ยนเป็น Scene 3
                     break;
                 case 3:
                     imgScene.setImageResource(R.drawable.scene_1_4);
-                    text.setText(R.string.scene_1_4);
+                    text.setText(R.string.Scene_1_4);
                     sceneIndex[0] = 4; // เปลี่ยนเป็น Scene 4
-                    break;
-                case 4:
-                    imgScene.setImageResource(R.drawable.scene_1_5);
-                    text.setText(R.string.scene_1_5);
-                    sceneIndex[0] = 5; // เปลี่ยนเป็น Scene 5
                     break;
                 default:
                     // หาก Scene เกิน 5 ทำให้ปุ่มไม่ทำอะไร หรือรีเซ็ตเป็น Scene 1
