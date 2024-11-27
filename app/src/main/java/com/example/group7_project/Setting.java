3450a2fb464b415c5a7c0c202531321ec36d9fd0
PattarataThanaakkarasophon6509611940 <pattarata.tha@dome.tu.ac.th>
2024-11-27 13:54:06 +0700

    deleted some import

--- app/src/main/java/com/example/group7_project/Setting.java
+++ app/src/main/java/com/example/group7_project/Setting.java
@@ -2,14 +2,10 @@ package com.example.group7_project;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AlertDialog;
-
-import kotlinx.coroutines.channels.Send;
-
 
 public class Setting {
     private Activity activity;
